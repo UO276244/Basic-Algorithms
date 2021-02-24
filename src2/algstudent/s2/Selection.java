a1cdf1e7d490ef989b9a968e425cccc57735b0af
MartinBeltranDiaz <MartinBeltranDiaz@LAPTOP-GV3ITHS8>
2021-02-24 19:05:23 +0100

    Insertion, direct selection and bubble algorithms implemented.

---
+++ src2/algstudent/s2/Selection.java
@@ -0,0 +1,26 @@
+﻿package algstudent.s2;
+
+/* This program can be used to sort n elements with 
+ * a "bad" algorithm (quadratic). 
+ * It is the SELECTION */
+public class Selection extends Vector {
+	public Selection(int nElements) {
+		super(nElements);
+	}
+	
+	@Override
+	public void sort() {
+		int posMin;
+		
+		for(int i = 0; i < super.elements.length-1; i++) {
+			posMin = super.findPosMin(i);
+			super.interchange(i, posMin);
+			
+		}
+	}  
+	
+	@Override
+	public String getName() {
+		return "Selection";
+	} 
+} 
