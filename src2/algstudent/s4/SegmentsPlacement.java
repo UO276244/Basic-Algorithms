f15a66979263aafc44ded52b3bf08ad7af49686d
MartinBeltranDiaz <MartinBeltranDiaz@LAPTOP-GV3ITHS8>
2021-03-18 17:44:40 +0100

    Finished class for measuring times.

--- src2/algstudent/s4/SegmentsPlacement.java
+++ src2/algstudent/s4/SegmentsPlacement.java
@@ -10,23 +10,27 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import algstudent.s32.Inversions;
 
 public class SegmentsPlacement {
 
-	private static int numOfLines;
-	private static Map<String, Integer> listOfSegments;
+	private int numOfLines;
+	private  Map<String, Integer> listOfSegments;
 	
 	
-	public static void main(String args[]) {
+	public  void main(String filename) {
 		numOfLines = 0;
 		listOfSegments = new HashMap<String,Integer>();
-		importSegmentsFromFile(args[0]);
+		importSegmentsFromFile(filename);//"files/game1.txt"
 		
 		
-		Greedy1();
-		Greedy2();
-		Greedy3();
+		Greedy1(true);
+		Greedy2(true);
+		Greedy3(true);
+	}
+	
+	public SegmentsPlacement(Map<String,Integer> listOfElems, int numOfSegments) {
+		this.listOfSegments = listOfElems;
+		this.numOfLines = numOfSegments;
 	}
 	
 	
@@ -36,13 +40,13 @@ public class SegmentsPlacement {
 	 * because in the end every solution gives the same cost, 
 	 * then why are we going to complicate our lives?".
 	 */
-	private static void Greedy1() {
+	public void Greedy1(boolean toPrint) {
 		
 		List<String> sortedKeys= new ArrayList<String>(listOfSegments.keySet());
 		Collections.sort(sortedKeys);
 		//I copy the keys to a list and i order them from lower (S0) to gratest (S5)
 		
-		System.out.println("GREEDY 1:");
+		if(toPrint) {System.out.println("GREEDY 1:");}
 		double finalCost = 0.0;
 		double currentMaxPos = 0;
 		double movement = 0;
@@ -51,67 +55,73 @@ public class SegmentsPlacement {
 			movement = listOfSegments.get(key)+currentMaxPos;
 			currentMidPoint = movement - (movement - currentMaxPos)/2;
 			finalCost += currentMidPoint;
-			System.out.println(""+key+": (" + (int)currentMaxPos + " to " + (int)movement + "), midpoint = " + currentMidPoint);
+			if(toPrint) {
+				System.out.println(""+key+": (" + (int)currentMaxPos + " to " + (int)movement + "), midpoint = " + currentMidPoint);
+				
+			}
 			currentMaxPos = movement;
 		}
 		
-		System.out.println("Cost of greedy 1 = " + finalCost+"\n");
+		if(toPrint) {System.out.println("Cost of greedy 1 = " + finalCost+"\n");}
 	}
 	
 	
 	/**
 	 * GREEDY 2: If consists in placing them from longest to shortest length.
 	 */
-	private static void Greedy2() {
+	public void Greedy2(boolean toPrint) {
 		
-		System.out.println("GREEDY 2:");
+		if(toPrint) {System.out.println("GREEDY 2:");}
 		List<Integer> values = new ArrayList<Integer>(listOfSegments.values());
-		Inversions sorter = new Inversions(values);
-		sorter.mergeSort(0, values.size() - 1);
-		values = sorter.getRanking();
+		
+		Collections.sort(values);
 		
 		double finalCost = 0.0;
 		double currentMaxPos = 0;
 		double movement = 0;
 		double currentMidPoint;
-		for(int i = values.size() - 1; i>= 0; i--) {
+		for(int i = numOfLines - 1; i>= 0; i--) {
 			
 			movement = values.get(i)+currentMaxPos;
 			currentMidPoint = movement - (movement - currentMaxPos)/2;
 			finalCost += currentMidPoint;
-			System.out.println(""+values.get(i)+": (" + (int)currentMaxPos + " to " + (int)movement + "), midpoint = " + currentMidPoint);
+			if(toPrint) {
+				System.out.println(""+values.get(i)+": (" + (int)currentMaxPos + " to " + (int)movement + "), midpoint = " + currentMidPoint);
+				
+			}
 			currentMaxPos = movement;
 			
 		}
 		
-		System.out.println("Cost of greedy 2 = " + finalCost + "\n");
+		if(toPrint) {System.out.println("Cost of greedy 2 = " + finalCost + "\n");}
 	}
 	
 	/**
 	 * GREEDY 3: If consists in placing them from shortest to longest length.
 	 */
-	private static void Greedy3() {
+	public void Greedy3(boolean toPrint) {
 		
-		System.out.println("GREEDY 3:");
+		if(toPrint) {System.out.println("GREEDY 3:");}
 		List<Integer> values = new ArrayList<Integer>(listOfSegments.values());
-		Inversions sorter = new Inversions(values);
-		sorter.mergeSort(0, values.size() - 1);
+		Collections.sort(values);
 		
 		double finalCost = 0.0;
 		double currentMaxPos = 0;
 		double movement = 0;
 		double currentMidPoint;
-		for(int val : sorter.getRanking()) {
+		for(int val : values) {
 			
 			movement = val+currentMaxPos;
 			currentMidPoint = movement - (movement - currentMaxPos)/2;
 			finalCost += currentMidPoint;
+			if(toPrint) {
 			System.out.println(""+val+": (" + (int)currentMaxPos + " to " + (int)movement + "), midpoint = " + currentMidPoint);
+			}
 			currentMaxPos = movement;
 			
 		}
 		
-		System.out.println("Cost of greedy 3 = " + finalCost);
+		if(toPrint) {System.out.println("Cost of greedy 3 = " + finalCost);}
 		
 	}
 	
@@ -123,7 +133,7 @@ public class SegmentsPlacement {
 	 * Private method to read a file and import the number of segments and its corresponding length
 	 * @param fileName
 	 */
-	private static void importSegmentsFromFile(String fileName) {
+	private void importSegmentsFromFile(String fileName) {
 		
 		String line;
 		int lineCount = 0;
--- src2/algstudent/s4/SegmentsPlacementTimes.java
+++ src2/algstudent/s4/SegmentsPlacementTimes.java
@@ -1,5 +1,69 @@
 package algstudent.s4;
 
-public class SegmentsPlacementTimes {
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
 
+public class SegmentsPlacementTimes {
+	
+	private static int n;
+	private static int nTimes;
+	private static SegmentsPlacement segmentsAlgorithms;
+	private static Map<String,Integer> segmentsMap;
+	private static Random rand;
+	
+	
+	public static void main(String args[]) {
+		rand = new Random();
+		n = 100;
+		nTimes = 5;
+		long timeGreedy1;
+		long timeGreedy2;
+		long timeGreedy3;
+		System.out.println("N\t\t" + "Greedy1\t\t" + "Greedy2\t\t" + "Greedy3\t\t");
+		while(true) {
+		
+		
+			segmentsMap = createdMap(n);
+			segmentsAlgorithms = new SegmentsPlacement(segmentsMap, n);
+			
+			 timeGreedy1 = System.currentTimeMillis();
+			 for(int i = 0;i<nTimes;i++) {
+				 segmentsAlgorithms.Greedy1(false);
+			}
+			timeGreedy1 = (System.currentTimeMillis() - timeGreedy1)/nTimes;
+			
+			 timeGreedy2 = System.currentTimeMillis();
+			 for(int i = 0;i<nTimes;i++) {
+				 segmentsAlgorithms.Greedy2(false);
+			}
+			timeGreedy2 = (System.currentTimeMillis() - timeGreedy2)/nTimes;
+			
+			 timeGreedy3 = System.currentTimeMillis();
+			 for(int i = 0;i<nTimes;i++) {
+				 segmentsAlgorithms.Greedy3(false);
+			}
+			timeGreedy3 = (System.currentTimeMillis() - timeGreedy3)/nTimes;
+			
+			
+			
+			
+			
+			System.out.println(n + "\t\t" + timeGreedy1+"\t\t"+timeGreedy2+"\t\t"+timeGreedy3);
+			n*=2;
+		}
+		
+	}
+	
+	
+	private static HashMap<String,Integer> createdMap(int numOfElems){
+		
+		HashMap<String,Integer> hash = new HashMap<String,Integer>();
+		for(int i = 0; i<numOfElems;i++) {
+			hash.put("S"+i, rand.nextInt(100));
+		
+		}
+		
+		return hash;
+	}
 }
